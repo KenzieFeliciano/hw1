7cf2146a35f56c96130f11f10b3bfb010e9b0aad
Kenzie Feliciano <kgfeliciano20@gmail.com>
2025-09-13 17:37:52 -0700

    Final Split Function

--- split.cpp
+++ split.cpp
@@ -11,13 +11,35 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include<iostream>
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // get values into in. divy those into odds and evens
+
+  //case 1: in is empty so odds and even are also empty
+  if(in == nullptr){
+    odds = nullptr;
+    evens = nullptr;
+    return;
+  }
+
+  Node* next = in->next; //
+  //case 2: in is not empty
+  if(in->value % 2 ==0){ // if the value is even
+    evens = in; // value is now in evens list
+    split(next, odds, evens->next); // evens list built recursively. evens->next is next node in evens list
+  }
+  else{
+    odds = in; //create new node in odds list with that value
+    split(next, odds->next, evens); //recursively use split on next node from in. oods->next becomes the next node in odds list
+  }
+
+  in = nullptr; // prevent dangling pointers
+  return;
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,62 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+    //create a linked list (2, 3, 4, 5)
+    Node* head = new Node(2, nullptr);
+    head->next = new Node(3, nullptr);
+    head->next->next = new Node(4, nullptr);
+    head->next->next->next = new Node(5, nullptr);
 
+    //print the list
+    cout << "Original list: " << endl;
+    Node* curr = head;
+    while(curr != nullptr){
+        cout << curr->value << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+
+    //split the list. odds and evens start as nullptr
+    Node* odds = nullptr;
+    Node* evens = nullptr;
+    split(head, odds, evens);
+    head = nullptr; // so theres no dangling pointer
+    cout << "Updated lists: " << endl;
+
+    // first evens list
+    cout << "Evens: ";
+    curr = evens;
+    while(curr != nullptr){
+        cout << curr->value << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+
+    // now odds list
+    cout << "Odds: ";
+    curr = odds;
+    while (curr != nullptr){
+        cout << curr->value << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+
+    // clean up the memory
+    while(odds != nullptr){
+        Node* temp = odds;
+        odds = odds-> next;
+        delete temp;
+    }
+
+    while(evens != nullptr){
+        Node* temp = evens;
+        evens = evens->next;
+        delete temp;
+    }
+    return 0;
 }
